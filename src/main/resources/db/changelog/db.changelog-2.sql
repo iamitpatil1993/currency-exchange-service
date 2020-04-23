2fdc44d1fd5a02b917d685608e19d737e0ea121f
amipatil <iamitpatil1993@gmail.com>
2020-04-23 22:01:06 +0530

    Added liquibase script for currencies

--- src/main/resources/db/changelog/db.changelog-2.sql
+++ src/main/resources/db/changelog/db.changelog-2.sql
@@ -1,3 +1,5 @@
 --liquibase formatted sql
 --changeset amipatil:202004201050
 INSERT INTO currency_mapping values(1, 'USD', 'INR', 75, false, now(), now());
+INSERT INTO currency_mapping values(2, 'EUR', 'INR', 65, false, now(), now());
+INSERT INTO currency_mapping values(3, 'AUS', 'INR', 50, false, now(), now());
